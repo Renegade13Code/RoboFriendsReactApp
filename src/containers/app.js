9dc320a42b092cfdda81b79c3f33791038f49b1b
Brandon <brandon16scott@gmail.com>
2022-06-21 13:47:15 +0200

    Converted App component to function with hooks

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, useState, useEffect} from 'react';
 // import { robots } from './robots'
 import CardList from '../components/cardList';
 import SearchBar from '../components/SearchBar';
@@ -6,64 +6,54 @@ import './app.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/errorBoundary';
 
-class app extends React.Component{
+function App(){
 
-    constructor(){
-        super();
-        this.state = {
-            robots: [],
-            searchField: ""
-        };
-        console.log("constructor");
-    }
+    //This is called destructuring
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState("");
 
-    //This is a lifecycle method that is overridden(https://reactjs.org/docs/react-component.html) 
-    componentDidMount(){
-        console.log("componentDidMount");
+    //Passed function will execute everytime render completes. If you inspect you will see that useEffect executes continuously,
+    //this is because function passed updates the state which causes the render to occur again and so on and so forth. You can 
+    // optionally pass a second parameter to avoid this. React will compare the current value of the passed array to the value it was
+    // in the previous render, it will only rerender if the value has changed. https://reactjs.org/docs/hooks-effect.html
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users/")
             .then(response => {
                 return response.json();
             })
             .then(users => {
-                this.setState({robots: users});
+                setRobots(users);
                 console.log(users);
             })
-    }
+    }, []);
 
-    //NB: When adding a function to a component class that does not override the parent class, must use arrow 
-    //function. This ensures that any this call within the function refers to the class it is declared in and 
-    //not the parent element the function is called from i.e. from within SearchBar component were it is passed. 
-    onChangeEventHandler = (event) => {
-        this.setState({searchField: event.target.value});
+    const onChangeEventHandler = (event) => {
+        setSearchField(event.target.value);
     }
-
-    render(){  
-        //This is called destructuring
-        const {robots, searchField} = this.state;
-        const filteredRobots = robots.filter((robot)=>{
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        })
-        console.log("render");
-        
-        if(robots.length === 0){
-            return <h1 className='tc'>Loading</h1>
-        }
-        else{
-            return(
-                <div className='tc container'>
-                    <div className='header'>
-                        <h1 className='f1'>RoboFriends</h1>
-                        <SearchBar onChangeEvent={this.onChangeEventHandler}/>
-                    </div>
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots}/>
-                        </ErrorBoundary>
-                    </Scroll>
+  
+    const filteredRobots = robots.filter((robot)=>{
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    console.log("render");
+    
+    if(robots.length === 0){
+        return <h1 className='tc'>Loading</h1>
+    }
+    else{
+        return(
+            <div className='tc container'>
+                <div className='header'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBar onChangeEvent={onChangeEventHandler}/>
                 </div>
-            );
-        }
+                <Scroll>
+                    <ErrorBoundary>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundary>
+                </Scroll>
+            </div>
+        );
     }
 }
 
-export default app;
+export default App;
